0460f00f4eb48265b0b6c3cc8b87129fbb20aca1
acetheman <nolasco.hermen@gmail.com>
2023-06-26 08:14:15 -0500

    updated html

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const accordion = document.getElementsByClassName('content-container');
 
 for (i = 0; i < accordion.length; i++) {
 
-    accordion[i].addEventListener('click', function(){
+    accordion[i].addEventListener('click', function() {
         this.classList.toggle('active');
     })
 };
